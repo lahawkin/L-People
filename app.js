4481fba30a6a507947a8c0423b5adf3a820fa630
lahawkin <lhawk1000@gmail.com>
2019-11-22 11:26:31 -0800

    got the tests passing

---
+++ app.js
@@ -0,0 +1,6 @@
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager"); 
+
+
+const tmp = new Intern("jon", 1, "yo@yo.com", "UCSD"); 
