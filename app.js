025650df9388b15bfe0baadeeb2a8e35b42b9c1e
lahawkin <lhawk1000@gmail.com>
2019-11-25 16:19:52 -0800

    got a lot working

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const inquirer = require("inquirer");
-//const helper = require("./helper");
-
+const fileSystem = require('fs');
 
 
 //okay so here is where we get started with the inquirer thing
@@ -30,9 +29,6 @@ questions_for_all_employees = [
     }
 
 ]
-
-answers = [];
-
 managers_questions = [
     {
         type: "input",
@@ -40,82 +36,92 @@ managers_questions = [
         message: "What is their office number?",
     }
 ]
-
+engineer_questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "What is their github?",
+    }
+]
+intern_question = [
+    {
+        type: "input",
+        name: "school",
+        message: "What school do they go to?",
+    }
+]
 
 function employeeChoice() {
     inquirer.prompt(questions_for_all_employees)
         .then(val => {
-            console.log(val);
-            console.log(val.type);
+            const fs = fileSystem;
+            const templateFile = fs
+                .readFileSync('./templates/main.html', { encoding: 'utf8' });
+            fs.writeFileSync("./output/final.html", templateFile, 'utf8');
             switch (val.choice) {
-                case "Manager":
+                case "manager":
                     manager(val);
-
                     break;
-                case "Engineer":
+                case "engineer":
                     engineer(val);
-                    //get some more questions
-                    //add it to the html
                     break;
-                case "Intern":
+                case "intern":
                     intern(val);
-                    //get some more questions
-                    //add it to the html
                     break;
                 default:
                     break;
             }
-            //
         });
-
-
 }
-employeeChoice();
 
 function intern(val) {
-    const varIn = new Intern(val.name, val.idNum, val.email);
-    console.log("Intern");
-    //TODO get more questions
-    //inquirer.prompt(){
-
+    inquirer.prompt(intern_question).then(extra => {
+        var varIntern = new Intern(val.name, val.idNum, val.email, extra.school);
+        const fs = fileSystem;
+        const templateFile = fs
+            .readFileSync('./templates/intern.html', { encoding: 'utf8' });
+        let temporaryFile = templateFile.replace('{{name}}', varIntern.name);
+        temporaryFile = temporaryFile.replace('{{role}}', varIntern.getRole());
+        temporaryFile = temporaryFile.replace('{{id}}', varIntern.id);
+        temporaryFile = temporaryFile.replace('{{email}}', varIntern.email);
+        temporaryFile = temporaryFile.replace('{{school}}', varIntern.getSchool());
+        fs.appendFile("./output/final.html", temporaryFile, err => {
+            console.log(err)
+        });
+        console.log("temp file", temporaryFile);
+    });
 }
 
-
 function engineer(val) {
-    const varEng = new Engineer(val.name, val.idNum, val.email);
-    console.log("Engineer");
-    //TODO get more questions
+
+    inquirer.prompt(engineer_questions).then(extra => {
+        const varEng = new Engineer(val.name, val.idNum, val.email, extra.github);
+        const fs = require('fs');
+        const templateFile = fs
+            .readFileSync('./templates/engineer.html', { encoding: 'utf8' });
+        let temporaryFile = templateFile.replace('{{id}}', varEng.id);
+        temporaryFile = temporaryFile.replace('{{role}}', varEng.getRole());
+        temporaryFile = temporaryFile.replace('{{email}}', varEng.email);
+        temporaryFile = temporaryFile.replace('{{github}}', varEng.email);
+        fs.appendFile("./output/final.html", temporaryFile, 'utf8');
+        console.log("temp file", temporaryFile);
+    });
 }
 
 function manager(val) {
-
     inquirer.prompt(managers_questions).then(extra => {
-        const varMan = new Manager(val.name, val.idNum, extra.officeNumber, val.email)
-        console.log("Manager");
-
-        //TODO get some more questions
-        writeFile();
+        const varMan = new Manager(val.name, val.idNum, val.email, extra.officeNumber)
+        const fs = require('fs');
+        const templateFile = fs
+            .readFileSync('./templates/manager.html', { encoding: 'utf8' });
+        let temporaryFile = templateFile.replace('{{role}}', varMan.getRole);
+        temporaryFile = temporaryFile.replace('{{id}}', varMan.id);
+        temporaryFile = temporaryFile.replace('{{email1}}', varMan.email);
+        temporaryFile = temporaryFile.replace('{{email2}}', varMan.email);
+        temporaryFile = temporaryFile.replace('{{officeNumber}}', varMan.getOffice());
+        fs.appendFile("./output/final.html", temporaryFile, 'utf8');
+        console.log("temp file", temporaryFile);
     });
-
 }
 
-function writeFile() {
-
-    const fs = require('fs');
-
-    const templateFile = fs
-        .readFileSync(
-            './templates/manager.html',
-            { encoding: 'utf8' }
-        );
-    let temporaryFile = templateFile.replace('{{id}}', '123');
-
-
-    temporaryFile = temporaryFile.replace('{{email1}}', 'Bob@subgenius.org')
-    temporaryFile = temporaryFile.replace('{{email2}}', 'Bob@subgenius.org')
-
-    temporaryFile = temporaryFile.replace('{{officeNumber}}', '3'); 
-    console.log("temp file", temporaryFile);
-}
-//employeeChoice();
-writeFile(); 
+employeeChoice();
