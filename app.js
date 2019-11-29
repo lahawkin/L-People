52864652db9557e86dd613b6ed726cdf9be04577
lahawkin <lhawk1000@gmail.com>
2019-11-28 16:00:49 -0800

    ready with the final project

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const inquirer = require("inquirer");
 const fileSystem = require('fs');
+const Employee = require("./lib/Employee");
 
 
 //okay so here is where we get started with the inquirer thing
@@ -69,18 +70,18 @@ add_to_team_question = [
 function start() {
     inquirer.prompt(team_question).then(val => {
         const fs = fileSystem;
-        console.log(val);
-        console.log(val.team); 
         const templateFile = fs
             .readFileSync('./templates/main.html', { encoding: 'utf8' });
         let temporaryFile = templateFile.replace('{{team}}', val.team);
         fs.writeFileSync("./output/final.html", temporaryFile, 'utf8');
 
         inputMembers();
+        
     })
 
 
 }
+
 
 function inputMembers() {
     var teamSize;
@@ -119,17 +120,16 @@ function intern(val) {
         const fs = fileSystem;
         const templateFile = fs
             .readFileSync('./templates/intern.html', { encoding: 'utf8' });
-        let temporaryFile = templateFile.replace('{{name}}', varIntern.name);
+        let temporaryFile = templateFile.replace('{{name}}', varIntern.getName());
         temporaryFile = temporaryFile.replace('{{role}}', varIntern.getRole());
-        temporaryFile = temporaryFile.replace('{{id}}', varIntern.id);
-        temporaryFile = temporaryFile.replace('{{email1}}', varIntern.email);
-        temporaryFile = temporaryFile.replace('{{email2}}', varIntern.email);
+        temporaryFile = temporaryFile.replace('{{id}}', varIntern.getId());
+        temporaryFile = temporaryFile.replace('{{email1}}', varIntern.getEmail());
+        temporaryFile = temporaryFile.replace('{{email2}}', varIntern.getEmail());
         temporaryFile = temporaryFile.replace('{{school}}', varIntern.getSchool());
         fs.appendFile("./output/final.html", temporaryFile, err => {
             console.log(err)
         });
-        //console.log("temp file", temporaryFile);
-
+        
         inputMembers();
     });
 }
@@ -141,11 +141,11 @@ function engineer(val) {
         const fs = require('fs');
         const templateFile = fs
             .readFileSync('./templates/engineer.html', { encoding: 'utf8' });
-        let temporaryFile = templateFile.replace('{{id}}', varEng.id);
-        temporaryFile = temporaryFile.replace('{{name}}', varEng.name);
+        let temporaryFile = templateFile.replace('{{id}}', varEng.getId());
+        temporaryFile = temporaryFile.replace('{{name}}', varEng.getName());
         temporaryFile = temporaryFile.replace('{{role}}', varEng.getRole());
-        temporaryFile = temporaryFile.replace('{{email1}}', varEng.email);
-        temporaryFile = temporaryFile.replace('{{email2}}', varEng.email);
+        temporaryFile = temporaryFile.replace('{{email1}}', varEng.getEmail());
+        temporaryFile = temporaryFile.replace('{{email2}}', varEng.getEmail());
         temporaryFile = temporaryFile.replace('{{github1}}', varEng.getGithub());
         temporaryFile = temporaryFile.replace('{{github2}}', varEng.getGithub());
 
@@ -164,14 +164,14 @@ function manager(val) {
         const templateFile = fs
             .readFileSync('./templates/manager.html', { encoding: 'utf8' });
         let temporaryFile = templateFile.replace('{{role}}', varMan.getRole);
-        temporaryFile = temporaryFile.replace('{{id}}', varMan.id);
-        temporaryFile = temporaryFile.replace('{{email1}}', varMan.email);
-        temporaryFile = temporaryFile.replace('{{email2}}', varMan.email);
+        temporaryFile = temporaryFile.replace('{{id}}', varMan.getId());
+        temporaryFile = temporaryFile.replace('{{email1}}', varMan.getEmail());
+        temporaryFile = temporaryFile.replace('{{email2}}', varMan.getEmail());
         temporaryFile = temporaryFile.replace('{{officeNumber}}', varMan.getOffice());
         fs.appendFile("./output/final.html", temporaryFile, err => {
             console.log(err)
         });
-        //console.log("temp file", temporaryFile);
+    
         inputMembers();
     });
 }
